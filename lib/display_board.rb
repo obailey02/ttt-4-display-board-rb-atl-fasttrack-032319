70ce21fe155ff110e5ad648b58b2d718b031aafe
Otis Bailey III <obailey04@yahoo.com>
2019-03-24 18:05:34 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+
+students = ["reina", "deka", "jess", "otis", "mary", "ronnie", "sharalyn", "jaylen", "alex", "jacoby", "deisha", "leland"]
+
+def random_pairs1(names)
+  
+end
+
+def make_pair(names)
